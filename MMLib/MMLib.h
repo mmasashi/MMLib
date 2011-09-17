c6a28d3e50ecb086903ea83c386d958267af8a5b
mmasashi <mmasashi@gmail.com>
2011-09-17 17:10:54 +0900

    Initial Commit

---
+++ MMLib/MMLib.h
@@ -0,0 +1,13 @@
+//
+//  MMLib.h
+//  MMLib
+//
+//  Created by Miyazaki Masashi on 11/09/17.
+//  Copyright 2011 mmasashi.jp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMLib : NSObject
+
+@end
---
+++ MMLib/MMLib.m
@@ -0,0 +1,23 @@
+//
+//  MMLib.m
+//  MMLib
+//
+//  Created by Miyazaki Masashi on 11/09/17.
+//  Copyright 2011 mmasashi.jp. All rights reserved.
+//
+
+#import "MMLib.h"
+
+@implementation MMLib
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
